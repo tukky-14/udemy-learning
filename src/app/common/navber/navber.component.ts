bd26d1243965ed0d18c6962c044cffa71ff1e545
Eiji Otsuki <bigoonage0104@gmail.com>
2020-10-25 13:28:39 +0900

    ヘッダーコンポーネントを追加

---
+++ src/app/common/navber/navber.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navber',
+  templateUrl: './navber.component.html',
+  styleUrls: ['./navber.component.scss']
+})
+export class NavberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
